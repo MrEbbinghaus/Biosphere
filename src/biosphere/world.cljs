fb48a44d3adc30225784b5c376c5362294668a8b
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2020-03-02 00:02:58 +0100

    Fix weird fill issue

--- src/biosphere/world.cljs
+++ src/biosphere/world.cljs
@@ -22,8 +22,8 @@
   (let [color (if water?
                 (q/lerp-color (q/color 170 255 150) (q/color 150 120 255) (* (q/pow config/water-level -1) value))
                 (q/lerp-color (q/color 90 150 150) (q/color 0 255 255) humidity))]
-    (q/with-fill color
-      (q/rect x y config/scale config/scale))))
+    (q/fill color)
+    (q/rect x y config/scale config/scale)))
 
 
 (defn draw-tiles [tiles]
