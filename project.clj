ce9725b664a76e5b88053def374a23a8075e0116
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2020-03-09 21:52:22 +0100

    Switch to figwheel-main

--- project.clj
+++ project.clj
@@ -5,29 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [quil "3.1.0"]
-                 [org.clojure/clojurescript "1.10.520"]]
+                 [org.clojure/clojurescript "1.10.597"]]
 
-  :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-figwheel "0.5.19"]]
-  :hooks [leiningen.cljsbuild]
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.3"]
+                                  [com.bhauman/rebel-readline-cljs "0.1.4"]]
+                   :resource-paths ["target" "resources"]
+                   :clean-targets ^{:protect false} ["target"]}}
 
-  :clean-targets ^{:protect false} ["resources/public/js"]
-  :cljsbuild
-  {:builds [; development build with figwheel hot swap
-            {:id "development"
-             :source-paths ["src"]
-             :figwheel true
-             :compiler
-             {:main "biosphere.core"
-              :output-to "resources/public/js/main.js"
-              :output-dir "resources/public/js/development"
-              :asset-path "js/development"}}
-            ; minified and bundled build for deployment
-            {:id "optimized"
-             :source-paths ["src"]
-             :compiler
-             {:main "biosphere.core"
-              :output-to "resources/public/js/main.js"
-              :output-dir "resources/public/js/optimized"
-              :asset-path "js/optimized"
-              :optimizations :advanced}}]})
+  :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
+            "fig:dev"   ["trampoline" "run" "-m" "figwheel.main" "-b" "biosphere" "-r"]
+            "fig:publish" ["trampoline" "run" "-m" "figwheel.main" "-O" "advanced" "-bo" "biosphere"]})
